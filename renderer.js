824d4eb6fa174240be41bdc748908a6428e65941
Fredrick Esposito <fredrickesposito@gmail.com>
2019-11-12 00:06:09 -0500

    Added 'Hello World' Electron app
    The app can be started using 'npm start' and just displays a window with no menu bar and Windows style minimize, maxmize, and close buttons. We may want to make these buttons platform dependent or otherwise figure out how to hide the menu bar without making a frameless window (couldn't get this to work for some reason).

---
+++ renderer.js
@@ -0,0 +1,45 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const remote = require('electron').remote;
+
+// When document has loaded, initialise
+document.onreadystatechange = () => {
+    if (document.readyState == "complete") {
+        handleWindowControls();
+    }
+};
+
+function handleWindowControls() {
+
+    let win = remote.getCurrentWindow();
+    // Make minimise/maximise/restore/close buttons work when they are clicked
+    document.getElementById('min-button').addEventListener("click", event => {
+        win.minimize();
+    });
+
+    document.getElementById('max-button').addEventListener("click", event => {
+        win.maximize();
+    });
+
+    document.getElementById('restore-button').addEventListener("click", event => {
+        win.unmaximize();
+    });
+
+    document.getElementById('close-button').addEventListener("click", event => {
+        win.close();
+    });
+
+    // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
+    toggleMaxRestoreButtons();
+    win.on('maximize', toggleMaxRestoreButtons);
+    win.on('unmaximize', toggleMaxRestoreButtons);
+
+    function toggleMaxRestoreButtons() {
+        if (win.isMaximized()) {
+            document.body.classList.add('maximized');
+        } else {
+            document.body.classList.remove('maximized');
+        }
+    }
+}
