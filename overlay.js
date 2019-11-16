1f10f8b9691e6882013177f881f9f85a5b226896
Fredrick Esposito <fredrickesposito@gmail.com>
2019-11-16 02:33:25 -0500

    Show card values in overlay
    Positions seem to be off for some reason.

--- overlay.js
+++ overlay.js
@@ -1,7 +1,52 @@
-// API CALLS 
-
+const remote = require('electron').remote;
+var fs = require('fs');
+const addon = require('./build/Release/addon');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const logger = require('electron-log');
+var overlayWindow = remote.getCurrentWindow();
+var overlayWindowEnabled = true;
+var jsonDataDragon = require('./set1-en_us.json');
+var cardCache = {};
+
+// When document has loaded, initialise
+document.onreadystatechange = () => {
+    if (document.readyState == "complete") {
+        createCardCache();
+        runOverlayWindow();
+    }
+};
+
+function getTopWindowInfo() {
+    addon.gettopwindow();
+    return fs.readFileSync('topWindow.txt').toString().split("\n");
+}
+
+function createCardCache() {
+    jsonDataDragon.forEach(card => cardCache[card.cardCode] = card);
+    logger.log('created card cache');
+}
+
+function runOverlayWindow() {
+    setInterval(function(){
+        var topWindowInfo;
+        if(overlayWindowEnabled) {
+            topWindowInfo = getTopWindowInfo();
+            if(topWindowInfo[0].includes("Legends of Runeterra")) {
+                overlayWindow.setOpacity(1);
+                overlayWindow.setPosition(parseInt(topWindowInfo[1]), parseInt(topWindowInfo[2]));
+                overlayWindow.setSize(parseInt(topWindowInfo[3]), parseInt(topWindowInfo[4]));
+                logger.log('getting expedition state');
+                getExpeditionsState(cardCache);
+            }
+            else {
+                overlayWindow.setOpacity(0);
+            }
+        }
+        else {
+            overlayWindow.setOpacity(0);
+        }
+    }, 1000);
+}
 
 function getPositionalRectangles(cardCache){
     var request = new XMLHttpRequest();
@@ -16,6 +61,7 @@ function getPositionalRectangles(cardCache){
         logger.log(gameState);
 
         var rectangles = json.Rectangles;
+        eraseCardValues();
         Object.values(rectangles).forEach(rect=>{
             var cardID = rect.CardID;
             var cardCode = rect.CardCode;
@@ -34,8 +80,22 @@ function getPositionalRectangles(cardCache){
 }
 
 function displayCardValue(cardValue, x, y){
-
+    var spanTag = document.createElement("span");
+    spanTag.innerHTML = cardValue;
+    spanTag.className = "cardvalue";
+    spanTag.style.position = "absolute";
+    spanTag.style.left = x + "px";
+    spanTag.style.top = y + "px";
+    document.body.appendChild(spanTag);
 }
+
+function eraseCardValues() {
+    var elements = document.getElementsByClassName("cardvalue");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
 function findCardValue(cardCode, cardCache){
     logger.log('trying to find card value');
     card = cardCache[cardCode];
--- renderer.js
+++ renderer.js
@@ -2,46 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const remote = require('electron').remote;
-var fs = require('fs');
-const logger = require('electron-log');
-
-//import cpp code
-const addon = require('./build/Release/addon');
-
 var win = remote.getCurrentWindow();
-var overlayWindow = win.webContents.browserWindowOptions.overlay;
-var overlayWindowEnabled = false;
-
-function getTopWindowInfo() {
-    addon.gettopwindow();
-    return fs.readFileSync('topWindow.txt').toString().split("\n");
-}
-
-var jsonDataDragon = require('./set1-en_us.json');
-var cardCache = {};
-jsonDataDragon.forEach(card => cardCache[card.cardCode] = card);
-logger.log('created card cache');
-
-//print top window title 5 seconds
-setInterval(function(){
-    var topWindowInfo;
-    if(overlayWindowEnabled) {
-        topWindowInfo = getTopWindowInfo();
-        if(topWindowInfo[0].includes("Legends of Runeterra")) {
-            overlayWindow.setOpacity(1);
-            overlayWindow.setPosition(parseInt(topWindowInfo[1]), parseInt(topWindowInfo[2]));
-            overlayWindow.setSize(parseInt(topWindowInfo[3]), parseInt(topWindowInfo[4]));
-            logger.log('getting expedition state');
-            getExpeditionsState(cardCache);
-        }
-        else {
-            overlayWindow.setOpacity(0);
-        }
-    }
-    else {
-        overlayWindow.setOpacity(0);
-    }
-}, 2000);
 
 // When document has loaded, initialise
 document.onreadystatechange = () => {
@@ -87,12 +48,10 @@ function handleWindowControls() {
         if(overlayDispBtn.textContent == "Enable Overlay"){
             overlayDispBtn.textContent = "Disable Overlay";
             status.innerHTML = "Overlay enabled.<br>It will be displayed in Legends of Runeterra.";
-            overlayWindowEnabled = true;
         }
         else {
             overlayDispBtn.textContent = "Enable Overlay";
             status.innerHTML = "Overlay disabled.";
-            overlayWindowEnabled = false;
         }
     })
 
