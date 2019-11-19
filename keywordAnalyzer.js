5ea1a5b6efd46a9ee4abf203ce6da8c9df7fc2e9
Fredrick Esposito <fredrickesposito@gmail.com>
2019-11-19 18:44:19 -0500

    Added caluclations for Tough, Double Attack, and Ephemeral

--- keywordAnalyzer.js
+++ keywordAnalyzer.js
@@ -2,14 +2,17 @@ const logger = require('electron-log');
 
 var calcFunctionsByKeyword = {
     "Can't Block": function(card) {return -1 * (card.attack + card.health)/2},
-    "Quick Attack": function(card) {return card.attack},
+    "Quick Attack": function(card) {return card.attack * 0.5},
     "Barrier": function(card) {return 2},
     "Regeneration": function(card) {return card.health},
     "Overwhelm": function(card) {return card.attack * 0.5},
     "Fearsome": function(card) {return card.health > 3? card.attack*0.8 : 1},
     "Elusive": function(card) {return card.attack * 0.5},
     "Challenger": function(card) {return card.attack * 0.5},
-    "Lifesteal": function(card) {return (card.attack*0.4 + card.health*0.6)}
+    "Lifesteal": function(card) {return (card.attack*0.4 + card.health*0.6)},
+    "Tough": function(card) {return card.health/3},
+    "Double Attack": function(card) {return card.attack},
+    "Ephemeral": function(card) {return card.cost*0.3}
 };
 
 function getKeywordsValue(card){
